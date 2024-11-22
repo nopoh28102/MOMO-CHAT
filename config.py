f2633ae2498416d831b5e72a6f98dd57f56b5f21
Mahmoud Nabih <150601942+nopoh28102@users.noreply.github.com>
2024-11-22 13:40:28 +0200

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SERVER_URL = "https://your-server-url.com"
+SERVER_URL = "https://web-production-57a9.up.railway.app"
 PAGE_ACCESS_TOKEN = "your-page-access-token"
-VERIFY_TOKEN = "your-verify-token"
+VERIFY_TOKEN = "MR-NOPOH"
